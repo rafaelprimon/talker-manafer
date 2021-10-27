afeb16dd7bb568512e804e7cac784837db04d957
rafaelprimon <rafaelfernandesprimon@gmail.com>
2021-10-27 11:13:23 -0300

    req 2 - endpoint talker:id - metodo get

--- talkerRoute.js
+++ talkerRoute.js
@@ -12,4 +12,17 @@ router.get('/', async (_req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile('./talker.json', 'utf-8');
+  try {
+    const getDataTalker = JSON.parse(talkers);
+    const getTalker = getDataTalker.find((talker) => talker.id === parseInt(id, 10));
+    if (!getTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(200).json(getTalker);
+  } catch (error) {
+    return res.status(500).end();
+  }
+});
+
 module.exports = router;
